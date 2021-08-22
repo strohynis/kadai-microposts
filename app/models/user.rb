6f2540b64a61a25fc9cabe0248d67990eb519788
yy07266 <y9yuron@gmail.com>
2021-08-22 10:59:35 +0000

    likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :microposts
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
@@ -25,6 +27,19 @@ class User < ApplicationRecord
 
   def following?(other_user)
     self.followings.include?(other_user)
+  end
+  
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorite?(micropost)
+    self.likes.include?(micropost)
   end
   
   def feed_microposts
